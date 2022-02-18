b611dcbbf394b72e5af4b51dd0d1ffb928346391
StefanyCandida <stefany.candida@gmail.com>
2022-02-18 10:46:05 -0300

    alterando setState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import React, { Component }  from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+  state = {   
+        n1:null,
+        n2:null,    
+        result:null 
+      }
 
-export default App;
+  remover = () => { 
+    this.setState(
+      {result: this.state.n1 - this.state.n2
+      })
+    }
+
+  add = () => { 
+    this.setState({result: this.state.n1 + this.state.n2 })}
+
+  multi = () => { 
+    this.setState({result: this.state.n1 * this.state.n2})
+  }
+
+  dividir = () => { 
+    this.setState({result: this.state.n1 / this.state.n2})
+  }
+
+   zerar = () => {this.setState({
+    n1:null,
+    n2:null,    
+    result:null})}
+
+  
+ 
+   handleChangeN1 = (e) => { 
+     this.setState({
+       n1: Number(e.target.value)
+      })
+    }
+
+    handleChangeN2 = (e) => { 
+       this.setState({
+         n2: Number(e.target.value)
+         })
+        }
+
+
+   render() {
+    let {result} = this.state
+    return(
+      <div ClassName="calculos">
+        
+        <div ClassName="calcular">
+          <h1>React Calc App</h1>
+         <h2>{result}</h2>
+          <input type="number" onChange={this.handleChangeN1}/> 
+
+
+          <input type="number" onChange={this.handleChangeN2}/> 
+        </div>
+                  
+
+      <div ClassName="calculo">        
+              
+        <button onClick={this.add}>+</button>
+        <button onClick={this.remover}>-</button>
+        <button onClick={this.multi}>*</button>
+        <button onClick={this.dividir}>/</button>
+        <button onClick={this.zerar}>C</button>
+      </div> </div>
+      
+     )}
+}

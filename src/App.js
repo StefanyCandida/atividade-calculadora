1a06998dfac1aee0c11147c6b7e9d82c5b7ec973
StefanyCandida <stefany.candida@gmail.com>
2022-03-10 14:08:36 -0300

    inserido Styled-Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,81 @@
 import React, { Component }  from 'react'
 import './App.css';
+import styled from "styled-components";
+
+const GlobalStyle = styled.div`
+ {
+    height: 150vh;
+    background-color: teal;
+    display: flex;
+    justify-content: space-evenly;
+  }
+
+`
+const Calculos = styled.div`
+{ 
+  height: 100vh;
+  width: 12vw;
+  display: flex;
+  justify-content: space-around;
+
+}
+`
+
+const Calculo = styled.div`{
+  height: 5vh;
+  width: 30vw;
+  background-color: moccasin ;
+}`
+const Calcular = styled.div`
+.cacular{
+  border:solid 1px red;
+  height: 15vh;
+  width: 15vw;
+  background-color: moccasin ;
+}`
+
+const Hone = styled.h1`
+height: 20vh; 
+font-size: 3rem; 
+text-align: center;`
+
+
+const Input = styled.input`{
+  height: 10vh;
+  width: 30vw;
+  font-size: 2rem;
+  text-align: center; 
+  }
+  ` 
+  const Button = styled.button`{
+  height: 5vh;
+  width: 6vw;
+  font-size: 1.5rem; 
+  background-color: moccasin;
+
+}
+`
+
+
+const Htwo = styled.h2`{
+  height: 5vh;
+  width: 30vw;
+  text-align: center;
+  font-size: 3rem; 
+  border-bottom: 5px solid;
+
+}
+
+`
+const Hthree = styled.h3`
+{
+  height: 5vh;
+  width: 30vw;
+  text-align: center;
+  font-size: 2rem;  
+  
+
+}`
 
 export default class App extends Component {
   state = {   
@@ -8,6 +84,8 @@ export default class App extends Component {
         result:null,
         sinal: null
       }
+     
+
 
   remover = () => { 
     if (this.state.n1 && this.state.n2 !== "null" || "") {
@@ -93,27 +171,29 @@ export default class App extends Component {
    
     
     return(
-      <div ClassName="calculos">
+      <GlobalStyle>
+      <Calculos>
         
-        <div ClassName="calcular">
-          <h1>React Calc App</h1>
+        <Calcular>
+          <Hone>React Calc App</Hone>
          
-          <input  onChange={this.handleChangeN1} value={this.state.n1} placeholder= "0"/> 
-          <h3>{this.state.sinal}</h3>
-          <input onChange={this.handleChangeN2} value={this.state.n2} placeholder= "0"/> 
-          <h3>=</h3>
-          <h2>{this.state.result}</h2>
-        </div>                 
-
-      <div ClassName="calculo">        
+          <Input  onChange={this.handleChangeN1} value={this.state.n1} placeholder= "0"/> 
+          <Hthree>{this.state.sinal}</Hthree>
+          <Input onChange={this.handleChangeN2} value={this.state.n2} placeholder= "0"/> 
+          <Calculo>        
               
-        <button onClick={this.add}>+</button>
-        <button onClick={this.remover}>-</button>
-        <button onClick={this.multi}>*</button>
-        <button onClick={this.dividir}>/</button>
-        <button onClick={this.zerar}>C</button>
-      </div> 
-      </div>
+        <Button onClick={this.add}>+</Button>
+        <Button onClick={this.remover}>-</Button>
+        <Button onClick={this.multi}>*</Button>
+        <Button onClick={this.dividir}>/</Button>
+        <Button onClick={this.zerar}>C</Button>
+      </Calculo> 
+          <Hthree>=</Hthree>
+          <Htwo>{this.state.result}</Htwo>
+        </Calcular>                 
+
       
+      </Calculos>
+      </GlobalStyle>
      )}
 }
